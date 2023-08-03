69a255c4422a32daf68340197191b45fc35f52f2
DominiqueSavin <savin.dominique@gmail.com>
2023-08-03 15:58:34 +0200

    fin premiere journée v2

--- src/main/java/com/formaclean/projetfilrouge/models/forms/JobCreateForm.java
+++ src/main/java/com/formaclean/projetfilrouge/models/forms/JobCreateForm.java
@@ -1,12 +1,17 @@
 package com.formaclean.projetfilrouge.models.forms;
 
+import com.formaclean.projetfilrouge.entities.Client;
 import jakarta.validation.constraints.NotBlank;
 
 import java.time.LocalDate;
 
-public record JobCreateForm() {
+public record JobCreateForm(
+        @NotBlank
+        LocalDate date,
 
-    @NotBlank
-    LocalDate date;
+        String comment,
+        Client client
+) {
+
 
 }
