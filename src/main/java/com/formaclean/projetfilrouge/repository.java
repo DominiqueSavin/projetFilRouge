0ec2970b0cdde99f36720d5ddc65fe83b8cae781
DominiqueSavin <savin.dominique@gmail.com>
2023-08-03 08:59:55 +0200

    first commit

---
+++ src/main/java/com/formaclean/projetfilrouge/repository.java
@@ -0,0 +1,2 @@
+package com.formaclean.projetfilrouge;public class repository {
+}
---
+++ src/test/java/com/formaclean/projetfilrouge/ProjetFilRougeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formaclean.projetfilrouge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetFilRougeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
